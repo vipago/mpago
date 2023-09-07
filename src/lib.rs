60de8f5e056df77c46609d534ff068250966afa4
JackSkelt <davedossantos978@gmail.com>
2023-09-07 15:00:34 -0300

    feat: import common

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,7 @@
-#![recursion_limit = "256"]
-use thiserror::Error;
-
 pub mod client;
+pub mod common;
 pub mod oauth;
 pub mod payer;
 pub mod payments;
 
 pub static API_BASE_URL: &str = "https://api.mercadopago.com";
-
-#[derive(Debug, Error)]
-pub enum APIError {
-    #[error("Credentials are requires")]
-    BadRequest,
-    #[error("Credentials are invalid")]
-    Unauthorized,
-    #[error("Not found")]
-    NotFound,
-    #[error("Internal Server Error")]
-    InternalServerError,
-
-    #[error("Error while sending request: {0}")]
-    RequestError(#[from] reqwest::Error),
-
-    #[error("Authorization failed: {0}")]
-    AuthorizationError(String),
-}
